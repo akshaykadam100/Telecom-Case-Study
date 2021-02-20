5b556db1b0d5bf719b3b4935ced75432fc3ba4c6
Akshay - Home PC <akshaykadam.100@gmail.com>
2021-02-20 23:34:02 +0530

    Removed Requests Library From Requirement Library

--- Customer_Analyzer/views.py
+++ Customer_Analyzer/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
-import requests
 from django.http import HttpResponse
+
 
 # Create your views here.
 def analyzer_homepage(request):
